6504e32739f5c1a5d678276bde07371777309344
Lev <levdjepko@gmail.com>
2022-10-21 13:43:55 -0700

    Update rock_paper_scissors.py
    added the choice function

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,6 @@
 import random
 
-random_pick = random.randint(0,2)
-if random_pick == 0:
-    computer_choice = 'scissors'
-elif random_pick == 1:
-    computer_choice = 'rock'
-else:
-    computer_choice = 'paper'        
+computer_choice =random.choice(['scissors','rock','paper'])
 
 user_choice = input('rock, paper, or scissors?\n')
 
@@ -22,4 +16,4 @@ elif ((user_choice == 'paper' and computer_choice == 'scissors') or
 (user_choice == 'rock' and computer_choice == 'paper') or
 (user_choice == 'scissors' and computer_choice == 'rock')):
     print ('Computer picks: ' + computer_choice)
-    print ('Computer wins')   
+    print ('Computer wins')  
