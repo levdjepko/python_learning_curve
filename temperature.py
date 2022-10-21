c6979f4a26c9a257cd5a99f47339896bb68e5341
Lev <levdjepko@gmail.com>
2022-10-20 20:03:38 -0700

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,10 @@
+
+temperature = 75
+raining = True
+
+if temperature > 85 or temperature < 50 or raining:    
+    print ('Stay inside')
+   
+else:
+    print ('The weather is great!')
+    print ('Get out and walk!')   
