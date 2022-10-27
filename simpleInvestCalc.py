4146a7cd888ba963bb8b8942369c89c7b26ba0d6
Lev <levdjepko@gmail.com>
2022-10-26 19:41:40 -0700

    Update simpleInvestCalc.py

--- simpleInvestCalc.py
+++ simpleInvestCalc.py
@@ -1,11 +1,11 @@
 # get user inputs
-initialAmount = input('Enter the initial investement amount\n')
-time = input('Enter the time in month for this investement\n')
-additionalMonthly = input('Enter the amount of monthly investment')
+initialAmount = input ('Enter the initial investement amount\n')
+time = input ('Enter the time in month for this investement\n')
+additionalMonthly = input ('Enter the amount of monthly investment')
 totalAmount = int(initialAmount)
 
 # calculate the investment over time
-for i in range(int(time)):
+for i in range (int(time)):
     totalAmount += totalAmount * 0.07 / 12 + additionalMonthly
 
 # show the result    
