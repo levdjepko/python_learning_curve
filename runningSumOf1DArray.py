04b70153471dc78336658766c67861d9297d928f
Lev <levdjepko@gmail.com>
2022-11-22 08:44:20 -0800

    Create runningSumOf1DArray.py

---
+++ runningSumOf1DArray.py
@@ -0,0 +1,6 @@
+    def runningSum(self, nums: List[int]) -> List[int]:
+        # declare a new List to return it
+        return_list = []
+        for i in range(len(nums)): # for loop with i as an iterator
+            return_list.append(nums[i] + sum(nums[0:i]))
+        return return_list    
