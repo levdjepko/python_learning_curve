83a97ed21d7dcb90d441e44eb9d7ac0b3e2eac47
Lev <levdjepko@gmail.com>
2023-01-11 10:16:13 -0800

    Create iterableSum.py

---
+++ iterableSum.py
@@ -0,0 +1,5 @@
+expenses = []
+# sum all the expenses for a week
+expenses = [10.50, 8, 5, 3, 15, 20, 2]
+total = sum(expenses) # <--- this is similar to using a for loop for the list
+print("you spent $", total, " this week", sep='')
