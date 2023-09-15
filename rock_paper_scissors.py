578c5d6b6969a73ff928aa0c3ca58d4d7e5337dc
Lev <levdjepko@gmail.com>
2023-09-15 14:49:08 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,7 +1,7 @@
 import random
 
-computer_choice =random.choice(['scissors','rock','paper'])
-user_choice = input('rock, paper, or scissors?\n')
+computer_choice =random.choice(['scissors', 'rock', 'paper'])
+user_choice = input('Select: rock, paper, or scissors?\n')
 
 if (computer_choice == user_choice):
     print ('Computer picks: ' + computer_choice)
@@ -11,8 +11,6 @@ elif ((user_choice == 'rock' and computer_choice == 'scissors') or
 (user_choice == 'scissors' and computer_choice == 'paper')):
     print ('Computer picks: ' + computer_choice)
     print ('Player wins!')
-elif ((user_choice == 'paper' and computer_choice == 'scissors') or 
-(user_choice == 'rock' and computer_choice == 'paper') or
-(user_choice == 'scissors' and computer_choice == 'rock')):
+else:
     print ('Computer picks: ' + computer_choice)
-    print ('Computer wins') 
+    print ('Computer wins')
