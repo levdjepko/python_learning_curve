ec76827e23d97021c59e94d20101ad7d8f250f8b
Lev <levdjepko@gmail.com>
2023-09-22 09:47:30 -0700

    Create peopleInSpace.py

---
+++ peopleInSpace.py
@@ -0,0 +1,12 @@
+# there is an open API that gets the people who are currently in space
+# we need to use an HTTP request to this open API
+
+import requests
+
+# API endpoint is http://api.open-notify.org/astros.json (http://open-notify.org/Open-Notify-API/People-In-Space/)
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json()
+print(json)
+
+for person in json['people']:
+    print (person['name'])
