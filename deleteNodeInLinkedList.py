f8fa2df02621ace3d636c9baf241ef372702b588
Lev <levdjepko@gmail.com>
2023-11-17 08:49:30 -0800

    Create deleteNodeInLinkedList.py

---
+++ deleteNodeInLinkedList.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/93/linked-list/553/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # look at the next node
+        # and make our node a copy of the next node
+        nextNode = node.next
+        
+        
+        node.val = nextNode.val
+        node.next = nextNode.next
