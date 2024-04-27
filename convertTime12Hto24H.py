9a92f04abfc167f8b3cbf29449d292702715e1e6
Lev <levdjepko@gmail.com>
2024-04-26 20:10:15 -0700

    Create convertTime12Hto24H.py

---
+++ convertTime12Hto24H.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # format the string
+    # from AM/PM format to military time
+    hour=int(s[:2])
+    timeOfDay=s[8:]
+    
+        
+    if timeOfDay == 'PM':
+        if hour != 12:  # 12 pm = 12:00
+            hour+=12 
+    if timeOfDay == 'AM' and hour == 12:
+        hour-=12     
+    # handle the missing 0 from a single digit times  
+    if len(str(hour)) == 1:
+        adjustedHour='0'+str(hour)   
+    else:
+        adjustedHour =str(hour)
+               
+    newTime=str(adjustedHour)+s[2:8]
+    return newTime
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
