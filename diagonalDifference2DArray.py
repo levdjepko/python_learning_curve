55efe7ee4f2a20281bcc37de51951ae8397854a2
Lev <levdjepko@gmail.com>
2024-04-27 14:58:35 -0700

    Create diagonalDifference2DArray.py

---
+++ diagonalDifference2DArray.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# 
+#
+# 
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+#
+
+def diagonalDifference(arr):
+    firstDiagonal = secondDiagonal = 0
+    
+    #print(arr)
+    for i in range(len(arr)):
+        firstDiagonal+= arr[i][i]
+        #print("First:", arr[i][i])
+        secondDiagonal+= arr[i][len(arr)- i - 1]
+        #print("Second:", arr[i][len(arr)- i - 1])
+    #print(firstDiagonal, secondDiagonal)    
+    return abs(firstDiagonal - secondDiagonal)    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
