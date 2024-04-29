7f0ba94a2959ea72f10b7e17ce597ca117f61723
Lev <levdjepko@gmail.com>
2024-04-29 10:46:24 -0700

    Create sumOfSquaresOfDigitsInInt.py

---
+++ sumOfSquaresOfDigitsInInt.py
@@ -0,0 +1,14 @@
+def sum_of_square_digits(n):
+    # The input 'n' is a positive integer.
+    # Convert the integer 'n' to a string to iterate over the digits.
+    # For each digit, convert it back to integer and calculate its square.
+    # Add the square to a running total.
+    # Return the total.
+    number_as_string = str(n)
+    result = 0
+    for i in range(len(number_as_string)):
+        result += int(number_as_string[i]) ** 2
+    return result    
+
+n = int(input())
+print(sum_of_square_digits(n))
