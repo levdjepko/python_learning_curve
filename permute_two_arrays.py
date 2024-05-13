ea655b7d0733b6a783151d819d59824d43ce14a7
Lev <levdjepko@gmail.com>
2024-05-13 13:31:17 -0700

    Create permute_two_arrays.py

---
+++ permute_two_arrays.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def twoArrays(k, A, B):
+    # permute the arrays A and B to get the sums of each pair A[i]+B[i] >= k if that's possible
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(len(A)):
+        # the arrays A and B are the same size
+        # get the sums
+        if A[i] + B[i] < k:
+            return 'NO'
+    return 'YES'         
+
