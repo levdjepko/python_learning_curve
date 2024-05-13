80b405434068fdefc298c2405021572df7d2cbce
Lev <levdjepko@gmail.com>
2024-05-13 16:28:59 -0700

    Create rolling_window_of_fixed_size.py

---
+++ rolling_window_of_fixed_size.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # implement rolling window of fixed size
+    count_of_matches = 0
+    print("s:", s) 
+    print("d:", d)
+    print("m", m)
+    for i in range(len(s) - m + 1):
+        if sum(s[i: i + m]) == d:
+            count_of_matches += 1
+    return count_of_matches    
+
+
