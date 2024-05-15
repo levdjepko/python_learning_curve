560b155770b1df14dc92d79690eb0c74d0bd0a43
Lev <levdjepko@gmail.com>
2024-05-15 14:31:23 -0700

    Update openWeatherAPI.py

--- openWeatherAPI.py
+++ openWeatherAPI.py
@@ -1,9 +1,9 @@
 # Weather API key:
 # a50abde1c87f4cbc931162529232509
-# This key expires often, though! Check for the new key in the account on weatherapi.com
+# TODO This key expires often, though! Check for the new key in the account on -> weatherapi.com
 import requests
 
-url = 'http://api.weatherapi.com/v1/current.json?key=a50abde1c87f4cbc931162529232509&q=98685&aqi=no'
+url = 'http://api.weatherapi.com/v1/current.json?key=a50abde1c87f4cbc931162529232509&q=98683&aqi=no'
 response = requests.get(url)
 city = response.json().get('location').get('name')
 temp = response.json().get('current').get('temp_f')
