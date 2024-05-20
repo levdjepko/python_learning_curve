631d3d2a34ef8f6a139b70b3fbec8119aea73a0e
Lev <levdjepko@gmail.com>
2024-05-20 08:44:28 -0700

    Create maximum_permiter_triangle.py

---
+++ maximum_permiter_triangle.py
@@ -0,0 +1,14 @@
+def maximumPerimeterTriangle(sticks):
+    sticks.sort(reverse=True)
+    return_triangle = []
+    # a triangle always has three sides, and the sum of any two of them should be longer than the third side
+    # thus, we just need to find three longest values, sum of any two of which is larger than the third
+    for i in range(len(sticks) - 2):
+        a = sticks[i]
+        b = sticks[i + 1]
+        c = sticks[i + 2]
+        if a + b > c and a + c > b and b + c > a:
+            return_triangle = [c, b, a]
+            return return_triangle
+    return [-1]    
+                
