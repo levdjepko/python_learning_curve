5463753eea0e26fdacf07df8772ce42215aae3f6
Lev <levdjepko@gmail.com>
2024-05-26 15:58:03 -0700

    Create closest_numbers.py

---
+++ closest_numbers.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def closestNumbers(arr):
+    arr.sort()
+    smallest_diff = arr[1] - arr[0]
+    new_array = []
+    for i in range(len(arr) - 1):
+        if arr[i + 1] - arr[i] < smallest_diff:
+            smallest_diff = arr[i + 1] - arr[i]
+            new_array.clear()
+        if arr[i + 1] - arr[i] == smallest_diff:
+            new_array.append(arr[i])
+            new_array.append(arr[i + 1])
+    return new_array        
