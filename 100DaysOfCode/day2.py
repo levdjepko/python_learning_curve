57ebb1c4320214742820d58eff91ea16c0a72d8c
Lev <levdjepko@gmail.com>
2024-10-04 21:01:51 -0700

    Create day2.py

---
+++ 100DaysOfCode/day2.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+price_per_person = bill * (1 + tip/100) / people
+
+print(price_per_person)
+
