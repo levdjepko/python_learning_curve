ec73bfc9a0f32077bc3818d417333cd3a61310d8
Lev <levdjepko@gmail.com>
2024-10-22 08:16:08 -0700

    Create day10_calculator.py

---
+++ 100DaysOfCode/day10_calculator.py
@@ -0,0 +1,22 @@
+def add(n1, n2):
+    return n1 + n2
+def substract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {}
+operations["+"] = add
+operations["-"] = substract
+operations["*"] = multiply
+operations["/"] = divide
+
+
+n1 = float(input("First number: \n"))
+operator = input("Operation: + - / * \n")
+n2 = float(input("Second number: \n"))
+
+result = operations[operator](n1, n2)
+print(result)
