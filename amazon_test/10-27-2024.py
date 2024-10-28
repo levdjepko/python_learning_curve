8bb576cb02a9e8027ea6d16ac20802b5f37c6a89
Lev <levdjepko@gmail.com>
2024-10-27 18:37:50 -0700

    Create 10-27-2024.py

---
+++ amazon_test/10-27-2024.py
@@ -0,0 +1,6 @@
+''' Two questions were asked - the signal like [1,2,3,4,5] that needs to be sent via channels (e.g. 2 of them) such that we have the largest sum of medians of the arrays. 
+Solution was to sort the array in descending order, then to use channels - 1 elements of them, and then calculate the rest of the elements in the reaminign channel, while also calculating its median.
+#
+The other question was about the amazon delivery and warehouses, it had an array of places where the deliveries must be performed, and d = x distance of max delivery distance
+'''
+# solved 1 14/15 (weird case 7) and 2 15/15
