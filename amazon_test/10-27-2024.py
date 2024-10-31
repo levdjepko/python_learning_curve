b59ef900bedd5e51033c1607630f46da50b89a35
Lev <levdjepko@gmail.com>
2024-10-31 12:38:27 -0700

    Update 10-27-2024.py

--- amazon_test/10-27-2024.py
+++ amazon_test/10-27-2024.py
@@ -6,3 +6,5 @@ The other question was about Amazon delivery and warehouses, it had an array of 
 # solved 1 14/15 (weird case 7) and 2 15/15
 
 # next piece was an invitation to a "Virtual Interview" on 10/28/2024
+
+# After a phone call with a recruiter, I was told that the only other interview remaining is the Final Round of the interview, a 5~ hour-long online interview with 4 different people. Scheduled to the middle of this month
