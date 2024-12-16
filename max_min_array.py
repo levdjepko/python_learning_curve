fadf2ab311748037b44fd6e812b76470e2ac1750
Lev <levdjepko@gmail.com>
2024-12-16 08:24:41 -0800

    Update max_min_array.py

--- max_min_array.py
+++ max_min_array.py
@@ -1,11 +1,13 @@
 def maxMin(k, arr):
+    
     # create an array of length k from arr
-    # such that it's smallest and largest elements are as close as possible
+    # such that its smallest and largest elements are as close as possible
+    
     arr.sort()
     max_difference = arr[len(arr) - 1] - arr[0]
     for i in range(len(arr) - k + 1):
         # new_array = arr[i : i + k]
-        #print ("new array:", arr[i : i + k])
+        # print ("new array:", arr[i : i + k])
         min_element = arr[i]
         max_element = arr[i + k - 1]
         difference = max_element - min_element
