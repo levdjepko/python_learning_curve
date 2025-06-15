3fddea42232385175185a93b94875598acd07571
Lev <levdjepko@gmail.com>
2025-06-15 08:30:01 -0700

    Update counter_game.py

--- counter_game.py
+++ counter_game.py
@@ -1,13 +1,13 @@
 def is_power_of_two(n):
-    # check if the number is 0 to begin with
-    # if not, then check that bitwise & is exacly 0
+    # Check if the number is 0 to begin with
+    # if not, then check that bitwise & is exactly 0
     # because any power of 2 has exactly one 1 in binary representation, and ...
-    # same number -1 is all 1's but the set bit
+    # same number -1 is all 1's, but the set bit
     return (n != 0) and (n & (n-1) == 0)
     
 def counterGame(n):
-    # we have to substract the next lowest power of 2
-    # and then we get to a point where the number is reduced to 1 by dividing it on 2
+    # We have to subtract the next lowest power of 2
+    # and then we get to a point where the number is reduced to 1 by dividing it by 2
     moves = 0
     while n != 1:
         exponent = math.log2(n)
