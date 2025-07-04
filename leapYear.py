d4175a681ad3bd2a4023da7f69fa8f9c0a1321eb
Lev <levdjepko@gmail.com>
2025-07-04 09:57:39 -0700

    Update leapYear.py

--- leapYear.py
+++ leapYear.py
@@ -1,6 +1,6 @@
 def is_leap_year(year):
-    # Write your code here. 
-    # Don't change the function name.
+    # this program returns the year type - leap or not
+    # 
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
