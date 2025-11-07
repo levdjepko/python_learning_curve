8f7b242bcd99cc19423e2933c7753d6d30c32ef9
Lev <levdjepko@gmail.com>
2025-11-07 08:04:21 -0800

    Update permute_two_arrays.py

--- permute_two_arrays.py
+++ permute_two_arrays.py
@@ -4,9 +4,8 @@ import random
 import re
 import sys
 
-
 def twoArrays(k, A, B):
-    # Permute the arrays A and B to get the sums of each pair A[i]+B[i] >= k if that's possible
+    # Permute the arrays A and B to get the sums of each pair A[i] + B[i] >= k if that's possible
     A.sort()
     B.sort(reverse=True)
     
