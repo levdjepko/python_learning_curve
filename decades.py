6306f313625e6ce7c2b6e49bf06a998ba5ba1837
Lev <levdjepko@gmail.com>
2026-01-14 09:14:23 -0800

    Update decades.py

--- decades.py
+++ decades.py
@@ -1,4 +1,4 @@
-# find out how many decades old the person is
+# find out how many decades-old a person is
 age = int(input('How old are you?\n'))
 decades = age // 10   # integer division - Python3 version
 remainder = age % 10
